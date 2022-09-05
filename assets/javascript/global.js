d2528c956d71214125c85e7fad0695a06fb4402d
ARTHUR VIEIRA BRUSKE <arthur.bruske@alunos.sc.senac.br>
2022-09-05 09:12:21 -0300

    JavaAtt

--- assets/javascript/global.js
+++ assets/javascript/global.js
@@ -16,6 +16,8 @@ const books = [
         id: 1,
         text: 'Endurance',
         img: 'https://images-na.ssl-images-amazon.com/images/I/51Entnc8cUL._SX441_BO1,204,203,200_.jpg',
+        button: 'Comprar',
+        link: '../../pagLivros/endurance.html',
         type: 'line-01'
     },
 
@@ -23,6 +25,8 @@ const books = [
         id: 2,
         text: 'O Homem De Giz',
         img: 'https://images-na.ssl-images-amazon.com/images/I/31KPmUV6FBL._SY498_BO1,204,203,200_.jpg',
+        button: 'Comprar',
+        link: '#',
         type: 'line-01'
     },
 
@@ -30,6 +34,8 @@ const books = [
         id: 3,
         text: 'O Cortiço',
         img: 'https://images-na.ssl-images-amazon.com/images/I/518IaZYte8L._SX328_BO1,204,203,200_.jpg',
+        button: 'Comprar',
+        link: '#',
         type: 'line-01'
     },
 
@@ -37,6 +43,8 @@ const books = [
         id: 4,
         text: 'O poder do habito',
         img: 'https://images-na.ssl-images-amazon.com/images/I/51Xby92J9KL._SY344_BO1,204,203,200_QL70_ML2_.jpg',
+        button: 'Comprar',
+        link: '#',
         type: 'line-01'
     },
 
@@ -44,6 +52,8 @@ const books = [
         id: 5,
         text: 'Cabeça Fria, Coração Quente Capa',
         img: 'https://images-na.ssl-images-amazon.com/images/I/41afp7FgH8L._SX346_BO1,204,203,200_.jpg',
+        button: 'Comprar',
+        link: '#',
         type: 'line-02'
     },
 
@@ -51,6 +61,8 @@ const books = [
         id: 6,
         text: 'Quando Ninguém Está Olhando Capa',
         img: 'https://images-na.ssl-images-amazon.com/images/I/51jQqNedq9L._SX258_BO1,204,203,200_QL70_ML2_.jpg',
+        button: 'Comprar',
+        link: '#',
         type: 'line-02'
     },
 
@@ -58,6 +70,8 @@ const books = [
         id: 7,
         text: 'Extraordinário',
         img: 'https://images-na.ssl-images-amazon.com/images/I/31FrF2A0gKL._SX258_BO1,204,203,200_QL70_ML2_.jpg',
+        button: 'Comprar',
+        link: '#',
         type: 'line-02'
     },
 
@@ -65,9 +79,10 @@ const books = [
         id: 8,
         text: 'Poder e manipulação',
         img: 'https://images-na.ssl-images-amazon.com/images/I/412PUfafFzL._SY344_BO1,204,203,200_QL70_ML2_.jpg',
+        button: 'Comprar',
+        link: '#',
         type: 'line-02'
     },
-
 ];
 
 // active function
@@ -92,12 +107,12 @@ document.querySelector('#ui5r6qlhla').addEventListener('click',
 
 var i;
 for(i = 0; i <= books.length; i++){
-    insertBooks(books[i]['text'], books[i]['img'], books[i]['type']);
+    insertBooks(books[i]['text'], books[i]['img'],  books[i]['button'], books[i]['link'], books[i]['type']);
 };
 
 // function generate
 
-function insertBooks(element, img, type){
+function insertBooks(element, img, button, link, type){
     var element = ((element === '') ? false : element);
     var img = ((img === '') ? false : img);
     var type = ((type === '') ? false : type);
@@ -108,6 +123,7 @@ function insertBooks(element, img, type){
             <div class='f0axtplhol'>
                 <img src=${img} alt='Livro - ${element}' />
                 <p>${element}</p>
+                <a href="${link}">${button}</a>
             </div>
             `
         );
