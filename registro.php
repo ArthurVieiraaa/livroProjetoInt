7ae93bd41d19247ecdf0b3147e81c7ab22d9083c
LUKAS HENDRIK CORDT <lukas.cordt@alunos.sc.senac.br>
2022-10-19 09:06:49 -0300

    botei limites no cpf e no telefone

--- registro.php
+++ registro.php
@@ -104,50 +104,58 @@
     </nav>
     <form action="" method="POST">
         <div class="main-registro">
-        <div class="card-registro">
-            <h1>REGISTRO</h1>
-            <br>
-            <div class="textofield">
-                <label for="nome">Nome</label>
-                <input type="text" name="nome" placeholder="Nome" value="<?php echo $nome?>">
-                <span class="obrigatorio">*<?php echo $nomeErr ?></span>
+
+            <div class="card-registro">
+                <h1>REGISTRO</h1>
+                <br>
+                <div class="textofield">
+                    <label for="nome">Nome</label>
+                    <input type="text" name="nome" placeholder="Nome" value="<?php echo $nome?>">
+                    <span class="obrigatorio">*<?php echo $nomeErr ?></span>
+                </div>
+                <div class="textofield">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" placeholder="Email" value="<?php echo $email?>">
+                    <span class="obrigatorio">*<?php echo $emailErr ?></span>
+                </div>
+                <div class="textofield">
+                    <label for="cpf">CPF</label>
+                    <input type="text" name="cpf" required id="cpf" placeholder="000.000.000-00" maxlength="14" autocomplete="off" value="<?php echo $cpf?>">
+                    <span id='span-error-cpf' style='color: red;'></span>
+                    
+
+                </div>
+                <div class="textofield">
+                    <label for="phone">Telefone</label>
+                    <input type="text" required id="phone" name="phone" placeholder="(00) 0 0000-0000" autocomplete="off" maxlength="15" value="<?php echo $telefone?>">
+                    <span id='span-error-phone' style='color: red;'></span>
+
+
+
+                </div>
+                <div class="textofield">
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" placeholder="Senha" value="<?php echo $senha?>">
+                    <span class="obrigatorio">*<?php echo $senhaErr ?></span>
+                </div>
+                <div class="textofield">
+                    <label for="senhaconf">Confirmar sua Senha</label>
+                    <input type="password" name="senhaconf" placeholder="Confirme sua Senha" value="<?php echo $senhaconf?>">
+                    <span class="obrigatorio">*<?php echo $senhaconfErr ?></span>
+                </div>
+                <div class="textofield">
+                    <label for="adm">Administrador</label>
+                    <input type="checkbox" name="adm" placeholder="" value="<?php echo $adm?>">
+                </div>
+                <div>
+                    <input class="btn-registro" type="submit" value="Cadastrar" name="cadastro">
+                    <span class="obrigatorio"><?php echo $msgErr ?></span>
+                </div>
             </div>
-            <div class="textofield">
-                <label for="email">Email</label>
-                <input type="text" name="email" placeholder="Email" value="<?php echo $email?>">
-                <span class="obrigatorio">*<?php echo $emailErr ?></span>
-            </div>
-            <div class="textofield">
-                <label for="cpf">CPF</label>
-                <input type="text" name="cpf" placeholder="CPF" value="<?php echo $cpf?>">
-                <span class="obrigatorio">*<?php echo $cpfErr ?></span>
-            </div>
-            <div class="textofield">
-                <label for="telefone">Telefone</label>
-                <input type="text" name="telefone" placeholder="Telefone" value="<?php echo $telefone?>">
-                <span class="obrigatorio">*<?php echo $telefoneErr ?></span>
-            </div>
-            <div class="textofield">
-                <label for="senha">Senha</label>
-                <input type="password" name="senha" placeholder="Senha" value="<?php echo $senha?>">
-                <span class="obrigatorio">*<?php echo $senhaErr ?></span>
-            </div>
-            <div class="textofield">
-                <label for="senhaconf">Confirmar sua Senha</label>
-                <input type="password" name="senhaconf" placeholder="Confirme sua Senha" value="<?php echo $senhaconf?>">
-                <span class="obrigatorio">*<?php echo $senhaconfErr ?></span>
-            </div>
-            <div class="textofield">
-                <label for="adm">Administrador</label>
-                <input type="checkbox" name="adm" placeholder="" value="<?php echo $adm?>">
-            </div>
-            <div>
-            <input class="btn-registro" type="submit" value="Cadastrar" name="cadastro">
-            <span class="obrigatorio"><?php echo $msgErr ?></span>
         </div>
-        </div>
-    </div>
     </form>
     
+
+    <script src="assets/javascript/cpf.js"></script>
 </body>
 </html>
