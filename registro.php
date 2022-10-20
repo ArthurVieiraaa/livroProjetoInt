e3f372b1f36bd88cbc4c46464894f6f9ec50bd21
ARTHUR VIEIRA BRUSKE <arthur.bruske@alunos.sc.senac.br>
2022-10-20 09:00:17 -0300

    Arrumando Registro2

--- registro.php
+++ registro.php
@@ -46,9 +46,9 @@
             echo 'Senha não coincide.';
         };
         if (empty($_POST['adm'])){
-            $adm = "nao";
+            $adm = "Não";
         } else {
-            $adm = "sim";
+            $adm = "Sim";
         }
 
         //Inserir no banco de dados
@@ -120,13 +120,13 @@
             </div>
             <div class="textofield">
                 <label for="cpf">CPF</label>
-                <input type="text" name="cpf" placeholder="CPF" value="<?php echo $cpf?>">
-                <span class="obrigatorio">*<?php echo $cpfErr ?></span>
+                <input type="text" name="cpf" required id='cpf' placeholder="000.000.000-00" autocomplete="off" maxlength="14" value="<?php echo $cpf?>">
+                <span id='span-error-cpf' style='color: #00c3ff;'>*<?php echo $cpfErr ?></span>
             </div>
             <div class="textofield">
                 <label for="telefone">Telefone</label>
-                <input type="text" name="telefone" placeholder="Telefone" value="<?php echo $telefone?>">
-                <span class="obrigatorio">*<?php echo $telefoneErr ?></span>
+                <input type="text" name="phone" required id="phone" placeholder="(00) 0 0000-0000" autocomplete="off" maxlength="15" value="<?php echo $telefone?>">
+                <span id='span-error-phone' style='color: #00c3ff;'>*<?php echo $telefoneErr ?></span>
             </div>
             <div class="textofield">
                 <label for="senha">Senha</label>
@@ -138,11 +138,11 @@
                 <input type="password" name="senhaconf" placeholder="Confirme sua Senha" value="<?php echo $senhaconf?>">
                 <span class="obrigatorio">*<?php echo $senhaconfErr ?></span>
             </div>
-            <div class="">
+            <div class="checkbox-adm">
                 <label for="adm">Administrador</label>
                 <input type="checkbox" name="adm" placeholder="" value="<?php echo $adm?>">
             </div>
-            <div class="row">
+            <div class="btn-div">
             <input class="btn-registro" type="submit" value="Cadastrar" name="cadastro">
             <span class="obrigatorio"><?php echo $msgErr ?></span>
         </div>
