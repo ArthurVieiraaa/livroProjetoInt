ddb3e5bddb75cd0f9c7533d9a467dc552d17951c
Arthur <avieirabruske@gmail.com>
2022-12-06 21:47:51 -0300

    botao personalizado

--- index.php
+++ index.php
@@ -72,6 +72,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style/estilo.css">
+    <link rel="stylesheet" href="assets/style/css2.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 	<link rel="shortcut icon" href="imgs/logoicon.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -115,9 +116,13 @@
                     <input type="password" required name="senha" placeholder="Senha">
                     <!-- <span class="obrigatorio">* <?php echo $senhaErr ?></span> -->
                 </div>
-                <button class="btn-login">Login</button>
+                <button class="liquid">Login</button> 
+                <a href="biblioteca.php">
+                    <span>Button</span>
+                    <div class="liquid"></div>   
+                </a> 
             </div>
-                </form>
+            </form>
         </div>
     </div>
 </body>
